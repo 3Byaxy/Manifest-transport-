28ed5eab1f675b8554b304062c17171cab07fed1
Manager Daniel <byaruhandagadaniel89@gmail.com>
2026-03-16 11:11:15 +0300

    fix admin login

--- admin.js
+++ admin.js
@@ -1,5 +1,7 @@
-const ADMIN_PASSWORD = CONFIG.adminPassword
-const SCRIPT_URL = CONFIG.scriptUrl
+// const ADMIN_PASSWORD = CONFIG.adminPassword
+// const SCRIPT_URL = CONFIG.scriptUrl
+const ADMIN_PASSWORD = "MKA@KIU2026!"
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzUT3qtUWoeQZWfZMgZ90qcfLVsIqoXoRrU3bZoYQvAYaSIJTYdxJSqNNRr1QG3Gta57A/exec"
 let allRecords = []
 
 function login() {
--- config.js
+++ config.js
@@ -1,9 +1,11 @@
-const CONFIG = {
-  adminPassword: "MKA@KIU2026!",
-  scriptUrl: "https://script.google.com/macros/s/AKfycbzUT3qtUWoeQZWfZMgZ90qcfLVsIqoXoRrU3bZoYQvAYaSIJTYdxJSqNNRr1QG3Gta57A/exec"
-}
-```
+// const CONFIG = {
+//   adminPassword: "MKA@KIU2026!",
+//   scriptUrl: "https://script.google.com/macros/s/AKfycbzUT3qtUWoeQZWfZMgZ90qcfLVsIqoXoRrU3bZoYQvAYaSIJTYdxJSqNNRr1QG3Gta57A/exec"
+// }
+// ```
 
-**Step 3.** Create a file called `.gitignore` in your project folder and put this inside:
-```
-config.js
+// **Step 3.** Create a file called `.gitignore` in your project folder and put this inside:
+// ```
+// config.js
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzUT3qtUWoeQZWfZMgZ90qcfLVsIqoXoRrU3bZoYQvAYaSIJTYdxJSqNNRr1QG3Gta57A/exec"
+const ADMIN_PASSWORD = "MKA@KIU2026!"
